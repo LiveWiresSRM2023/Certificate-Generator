bf73345badc26dbea0a7649cd2187e39a820588a
RajeshK1006 <rajesh.kofficialmail@gmail.com>
2024-03-23 22:39:35 +0530

    all files are added

---
+++ front-end/otpverification/authen.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, session
+import random
+
+app = Flask(__name__)
+app.secret_key = 'Rajeshk1006'
+
+@app.route("/")
+def home():
+    return render_template("mobile.html")
+
+@app.route("/front-end/otpverification/templates/getph", methods=['POST'])
+def get_ph():
+    num = request.form['number']
+    return num  # Or render another template for failure
